c578297d508dbc3de5df50653d635bbca862d583
Rehman-Maan <129269381+Rehman-Maan@users.noreply.github.com>
2025-05-15 02:41:41 +0500

    Update ViewAuthors.js

--- src/components/ViewAuthors.js
+++ src/components/ViewAuthors.js
@@ -6,7 +6,7 @@ function ViewAuthors() {
 
   useEffect(() => {
     // Fetch authors from the backend
-    axios.get('https://1c4f-111-68-109-251.ngrok-free.app/authors')
+    axios.get('https://dc-cep-1.onrender.com/authors')
       .then(response => setAuthors(response.data))
       .catch(error => console.error('Error fetching authors:', error));
   }, []);
